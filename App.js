30b13d896dfca39e5eb99a871512816ced109a79
“callmeweirdo” <ogbandav102@gmail.com>
2023-01-05 00:45:22 +0100

    doing a quick refresh on react native with expo and building an nft marketplace

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// ? react navigation imports
+// import { CreateStacNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// ? fonts import
+import { useFonts } from "expo-font";
+
+// ? Screen Imports
+import Home from "./Screens/Home";
+import Details from "./Screens/Details";
+
+// ? setting up the navigation
+const Stack = createStackNavigator();
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
   },
-});
+};
+
+const App = () => {
+  const [loaded] = useFonts({
+    IneterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    IneterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    IneterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    IneterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    IneterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+export default App;
